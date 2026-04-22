45d816409d9bbb4686d40e27a164195c70c3d5a2
Ignacio Durandez <idurandez@frba.utn.edu.ar>
2026-04-22 20:49:06 -0300

    subo todos los cambios que hicimos con seba en usuarios

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,8 +1,42 @@
 class Usuario{
-    constructor(id,nombreUsuario,password){
+    constructor(id,nombreUsuario,password, avisos = []){
         this.id = id;
         this.nombreUsuario = nombreUsuario;
         this.password = password;
+        this.avisos = avisos;
+    }
+    
+    cancelarTurno(turno, destinatario){
+        turno.estado = CANCELADO;
+        const notificacion = crearSegunEstadoTurno(turno);
+
+        if(destinatario === notificacion.medico){
+            notificarPaciente(turno,notificacion)
+        }else{
+            notificarMedico(turno,notificacion);
+        }
+        //destinatario.usuario.avisos.push(notificacion);
+        //destinatario.recibirNotificacion(noti);
+    }
+
+    reservarTurno(turno){
+        turno.estado = RESERVADO;
+        const notificacion = crearSegunEstadoTurno(turno);
+        notificarMedico(turno,notificacion);
+    }
+
+    confirmarTurno(turno){
+        turno.estado = CONFIRMADO;
+        const notificacion = crearSegunEstadoTurno(turno);
+        notificarPaciente(turno,notificacion);
+    }
+
+    notificarMedico(turno,notificacion){
+        turno.medico.usuario.avisos.push(notificacion);
+    }
+
+    notificarPaciente(turno,notificacion){
+        turno.paciente.usuario.avisos.push(notificacion);
     }
 
 }
